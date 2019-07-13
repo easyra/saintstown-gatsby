f162ba143902d44128db6268d707323624013858
ezra davis davis <ezra.rr.davis@gmail.com>
2019-07-13 12:29:48 -0700

    change file structure

--- src/components/LandingContainer.js
+++ src/components/LandingContainer.js
@@ -1,13 +1,9 @@
 import React from "react"
 import styled from "styled-components"
 import { graphql, useStaticQuery } from "gatsby"
-import Img from "gatsby-image"
-import CTA from "./CTA"
-import AutherNoteText from "./AuthorNoteText"
-import AuthorNoteText from "./AuthorNoteText"
-import CTAWrapper from "./CTAWrapper"
-import AuthorNote from "./AuthorNote"
-import QuotesContainer from "./QuotesContainer"
+import CTAWrapper from "./LandingContainer/CTAWrapper"
+import AuthorNote from "./LandingContainer/AuthorNote"
+import QuotesContainer from "./LandingContainer/QuotesContainer"
 
 const LandingContainer = () => {
   const data = useStaticQuery(graphql`
@@ -34,21 +30,3 @@ const LandingContainer = () => {
 }
 
 export default LandingContainer
-
-const StyledSection = styled.section`
-  max-width: 1000px;
-  width: 100%;
-  padding: 15px;
-  margin: 15px auto;
-  display: flex;
-  justify-content: space-between;
-  p {
-    width: 100%;
-    max-width: 450px;
-  }
-  @media screen and (max-width: 800px) {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
-import CTA from "../components/CTA"
+import CTA from "../components/LandingContainer/CTA"
 import LandingContainer from "../components/LandingContainer"
 
 export default () => (
