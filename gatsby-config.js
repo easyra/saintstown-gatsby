2f8d763d1cb9b9cd213fb310fdaff7466d8f648d
ezra davis davis <ezra.rr.davis@gmail.com>
2019-07-13 21:18:35 -0700

    added styled-components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
