aa723e9f8da624439d37a6a41bf497f8466d40c0
easyra <ezra.rr.davis@gmail.com>
2020-07-23 17:33:33 -0700

    typos fixed

--- src/components/SynopsisContainer/synopsisData.js
+++ src/components/SynopsisContainer/synopsisData.js
@@ -2,32 +2,32 @@ const completedFeatures = [
   {
     title: "PAINT",
     text:
-      "Paint is a coming-of-age drama that pits a lone teenage girl against failed scholastic hoop superstars in the hood as they re-live their glory days on the Mecca of all urban basketball courts (Kings Court).  With the help of a recovering addict and former professional basketball star, she is given an opportunity to show her game.  The story examines the obstacles, faith and determination needed to prove to others that you are worthy of a chance.  ",
+      "Paint is a coming-of-age drama that pits a lone teenage girl, against failed scholastic hoop superstars in the hood, as they re-live their glory days on the Mecca of all urban basketball courts (Kings Court).  With the help of a recovering addict and former professional basketball star, she is given an opportunity to show her game.  The story examines the obstacles, faith, and determination needed to prove to others that you are worthy of a chance.  ",
   },
   {
     title: "SHINING STAR",
     text:
-      "Shining Star is a character driven drama that follows a young man’s decision to drop out of seminary school to become a rock star over the strenuous objection of his preacher father.  His launch into fame begins the rise and fall of a preacher’s kid-turned star that is torn by the crisis of fate.  The story examines the conflict between what a person chooses to become and ultimately what a person is destined to become.",
+      "Shining Star is a character driven drama that follows a young man and his decision to drop out of seminary school, to become a rock star, over the strenuous objection of his preacher father.  His launch into fame begins the rise and fall of a preacher’s kid-turned star, that is torn by the crisis of fate.  The story examines the conflict between what a person chooses to become and ultimately who a person is destined to become.",
   },
   {
     title: "GIRL OF MY DREAMS",
     text:
-      "Girl of my Dreams is a romance feature about Michael Edward’s reoccurring dream of true love and the only clue to her identity is the scar on her index finger.  He searches all of Chicago for her, meeting two women with the infamous scar to find out neither one is she.  However, both women share a connection to his best friend who has had the scar since their childhood.  In the end, this is a story about the search for true love and that sometimes there is much more to love than what the eyes can see. ",
+      "Girl of my Dreams is a romance feature about Michael Edward and his reoccurring dream of true love, with the only clue to her identity being a scar on her index finger.  He searches all over Chicago for her, meeting two women with the infamous scar, only to discover neither one is his dream girl.  However, both women share connections to his best friend, who has had the scar since their childhood.  In the end, this is a story about the search for true love and that sometimes there is much more to love than what the eyes can see. ",
   },
   {
     title: "KINGDOM COME",
     text:
-      "Kingdom Come is a heist thriller lead by Kevin Johnson, head of security at Kingdom Hotel and godson of owner/gangster Frank Landini.  Kevin, along with 4 disgruntled female employees serving forced probation at the Hotel, devises a plan to infiltrate and steal away with over 25 million dollars of Kingdom's laundered money.  Although an inside job, getting in and out under the watchful eye of Frank’s right hand man, Christolphe, will prove to be no easy feat.",
+      "Kingdom Come is a heist thriller lead by Kevin Johnson, head of security at Kingdom Hotel, and godson of owner/gangster Frank Landini.  Kevin, along with 4 disgruntled females, serving forced probation employment at the Hotel, devise a plan to steal over 25 million dollars of Kingdom's laundered money.  Although an inside job, getting in and out under the watchful eye of Frank’s right hand man, Christolphe, will prove to be no easy feat.",
   },
   {
     title: "FORGOTTEN LESSONS",
     text:
-      "Forgotten Lessons is about a young woman who wants to know who she is, how to love and be loved. So she revisits her past lives with the help of her granny’s Gullah friend where she has to learn the life lessons she already experienced, so she can live and fully love today. ",
+      "Forgotten Lessons is about a young woman who wants to know who she is, how to love and to be loved. So, she revisits her past lives, with the help of her granny’s Gullah friend, where she must relearn life lessons already experienced, so she can live and fully love today. ",
   },
   {
     title: "A LIFE’S RUN",
     text:
-      "A life's Run a crime and family drama about a gifted family of track stars who is hit with a tragedy when one of their siblings, who is estranged, is murdered. Unaware she gave birth to a daughter that was placed in foster care and grew up a troubled teen.  Presently,  in the act of committing a crime and the witness of another; she is reunited via a random car accident with her Aunt while running from the scene.  They instantly feel a connection.  Due to her sister’s death, the aunt’s passion for running was never the same until now. They are unaware of their relationship but share a love for running which forms a bond and soon the discovery of what else they have in common. It is an uplifting story for those who believe despite life “lifing” you; you should continue to run the race.",
+      "A Life's Run, a crime and family drama, about a family of gifted track stars who is hit with tragedy, when their estranged sister, is murdered. Unaware the sister had a daughter that grew up a troubled teen in foster care.  Seventeen years later the daughter, in the act of committing a crime and being witness to another, unknowingly reunited with her mother’s sister, while running from the scene via a random car accident.  The aunt’s passion for running was never the same after her sister’s death, until now. Unaware of their relationship; they instantly feel a connection.  They share a love for running.  It is an uplifting story for those who believe despite life “lifing” you; you should continue to run the race.",
   },
 ]
 
@@ -35,17 +35,17 @@ const completedTelevisionPilots = [
   {
     title: "ON THE AVE. one- hour drama",
     text:
-      "On the heels of his father’s heart attack, a prodigal son and Urban Music executive returns home and decide to take over a nightclub in his old dilapidated, drug and gang-infested neighborhood…Good luck! ",
+      "On the heels of his father’s heart attack, a prodigal son an Urban Music executive, returns home and decides to take over a nightclub in his old, dilapidated, drug and gang-infested neighborhood…Good luck!  ",
   },
   {
     title: "GREYSTONE LAW – one- hour drama",
     text:
-      "In the spirit of every movie where the wrong man is convicted of a crime and sent to jail, and the lawyer champions his cause, is a television series.  That series is Greystone.  Two attorneys operating out of an urban Chicago greystone specializes in representing wrongfully convicted criminals.  Pursuing cases often triggered by newspaper articles or sobbing, desperate mothers, these two protagonists from different times, races and environments, along with their three aides, take us in and out of the minds of clients through their investigations we’re never quite sure where justice or injustice begins.",
+      "In the spirit of every movie where the wrong man is convicted of a crime and sent to jail, and the lawyer champions his cause, is a television series.  That series is Greystone.  Two attorneys operating out of an urban Chicago greystone specialize in representing wrongfully convicted criminals.  Pursuing cases often triggered by newspaper articles or sobbing, desperate mothers, these two protagonists from different times, races, and environments, along with their three aides, take us in and out of the minds of clients through their investigations. We are never quite sure when justice or injustice begins.",
   },
   {
     title: "RAE OF LIGHT- dramedy",
     text:
-      "Rae has recently come to the realization that much of true love is about enjoying the person you meet rather than hoping for what they might become.  She has mixed feelings between wanting a man and not neglecting her responsibilities at work.  How does a busy 30-something, extremely creative photographer fit in dating?  With a little help from her “village” of friends, a lot of trial and error and her intimate, profound and humorous impromptu conversations with God about any and everything regarding her life, she is destined to figure it out. ",
+      "Rae has recently come to the realization that true love is about enjoying the person you meet rather than hoping who they might become.  She has mixed feelings about wanting a man and neglecting her responsibilities at work.  How does a busy, extremely creative, 30-something photographer fit dating in?  With a little help from her “village” of friends, a lot of trial and error and her intimate, profound, and humorous impromptu conversations with God. She is destined to figure it out. ",
   },
 ]
 
@@ -53,7 +53,7 @@ const completedShortFilm = [
   {
     title: "LOVE DEJA VU",
     text:
-      "Imagine making the wrong decision regarding love in a bar in 1942 to have your spirit relive itself in a different body, in the same bar and with the same character of spirits, only to make the wrong decision again in 1972.  Finally, you get it right in the exact same bar, with the same gathering of spirits, again in 2002. ",
+      "Imagine in 1942, making the wrong decision regarding love in a Harlem bar.  The same bar thirty years later, your spirit relives itself in a different body, only to make the wrong decision again.  2002… In the exact same bar, with the same gathering of spirits, they finally get it right.  ",
   },
 ]
 
@@ -61,12 +61,12 @@ const completedStagePlays = [
   {
     title: "BROTHERS",
     text:
-      "In every major city there are urban barbershops where Black men come together to celebrate, discuss, politicize, criticize, and analyze the social, economic, spiritual, and sexual experience of its people.  A play with soulful 70’s music playing throughout, Mack is the owner of such a barbershop.  He has no sons but is a surrogate father to the fatherless kids in the neighborhood.   Brothers, is an intimate story of a barbershop widower and two sons, one son, a gangster, ignored, never knowing who his father was and the other son, a college student, raised with his father but neither one knowing anything about the other.  Mack, along with his disciples, tries to correct some of the misunderstood relationships fathers have with their sons and sons have with their fathers while ministering to both his adopted sons about the relationship they must have with their heavenly father before tragedy strikes.",
+      "A play with soulful, Hip Hop and R&B 70’s, 80’s and 90’s music playing throughout.  In every major city there are urban barbershops where Black men come together to celebrate, discuss, politicize, criticize, and analyze the social, economic, spiritual, and sexual experience of its people. Mack is the owner of such a barbershop.  He has no children but is a surrogate father to the fatherless kids in the neighborhood.   Brothers is an intimate story of Mack a widower and two “sons;” one, a gangster, ignored, never knowing who his biological father was, and the other, a college student, raised with his father.  Mack, along with his disciples, tries to correct some of the misunderstood relationships’ fathers have with their sons and sons with their fathers. Mack ministering to both his adopted sons about the relationship they must have with their heavenly father before tragedy strikes.",
   },
   {
     title: "SIMON AND SKY",
     text:
-      "a skid-row musical is homage to and therefore based on, the well-known play and opera “Porgy and Bess.” This musical has a powerful social and spiritual message concerning doing something for the kingdom, being a blessing to others, to reach out before backing away and simply SEE people.",
+      "a skid-row musical pays homage to, and is based on the well-known play and opera “Porgy and Bess.” This musical has a powerful social and spiritual message: do something for the kingdom, be a blessing to others, reach out before backing away and simply SEE people.",
   },
 ]
 const data = {
