276a88835439b99a3e24deb2114e89117dfd1e33
easyra <ezra.rr.davis@gmail.com>
2020-08-07 10:30:05 -0700

    changed synopsis page

---
+++ src/components/SynopsisContainer/SynopsisList.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import {
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Collapse,
+  ListSubheader,
+} from "@material-ui/core"
+import ExpandLess from "@material-ui/icons/ExpandLess"
+import ExpandMore from "@material-ui/icons/ExpandMore"
+import { useState } from "react"
+
+const SynopsisList = ({ title, text }) => {
+  const [open, setOpen] = useState(false)
+  const handleClick = () => {
+    setOpen(state => !state)
+  }
+  return (
+    <>
+      <ListItem
+        button
+        onClick={handleClick}
+        style={{ background: "#8A1274", color: "#fff" }}
+      >
+        <ListItemText primary={title} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <p style={{ padding: "0 15px" }}>{text}</p>
+      </Collapse>
+    </>
+  )
+}
+
+export default SynopsisList
