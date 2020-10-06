8efcbae687476a6b9522637af515c1e556c95707
easyra <ezra.rr.davis@gmail.com>
2020-10-06 11:06:55 -0700

    podcast img added

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -1,13 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout"
 import StyledSection from "../components/@shared/StyledSection"
+import imgsrc from "../img/redcourt.jpg"
+import StyledButton from "../components/@shared/StyledButton"
 
 export default () => {
   return (
     <Layout>
       <StyledSection>
-        <h2>The Red Court</h2>
-        <p>Coming Soon...</p>
+        <img src={imgsrc} style={{ width: 250 }} />
+        <StyledButton href="https://www.youtube.com/channel/UCsjHErXdLpfhb33LTBBqLwg?fbclid=IwAR3an3Q8cESpnQRRV1Fk1-39_8co8QrbnyXmJFTcazkyKCUExb4XU-MF15M">
+          YouTube
+        </StyledButton>
       </StyledSection>
     </Layout>
   )
